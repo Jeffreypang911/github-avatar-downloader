935e19aad4bcd9ab4717162d971ec68c5f387945
jeffreypang911 <jeffreypang911@gmail.com>
2018-09-04 04:33:41 +0000

    working download avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,52 @@
 var request = require('request');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+var secret = require('./secrets.js')
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  console.log("Errors: ", err);
-  console.log("Results", result);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+      'User-Agent': 'request',
+      'Authorization': 'secret'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
 }
+
+
+
+function downloadImageByURL(url, filePath) {
+ request.get(url)
+
+  .on('error', function(err) {
+    console.log('error')
+  })
+
+  .on('response', function(response) {
+
+    console.log('Response Status Code: ' + response.statusCode);
+  })
+
+  .on('end', function(response) {
+    console.log("Downloaded")
+  })
+
+  .pipe(fs.createWriteStream('./' + filePath))
+
+}
+
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  // console.log("Errors:", err);
+  var url = "";
+  for (var i=0; i<result.length; i++){
+    url = result[i].avatar_url
+  downloadImageByURL(url, "avatar/" + result[i].login + ".jpg");
+}
+});
