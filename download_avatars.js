f564f076d9de6fbcbf559b2b560edee1002262d5
jeffreypang911 <jeffreypang911@gmail.com>
2018-09-04 02:37:49 +0000

    added function to download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,8 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  console.log("Errors: ", err);
+  console.log("Results", result);
+}
